0b08902a2ba0a6ee25d2111ce5e8d2b5d9f11d1e
ebedoyaalzate <esteban.bedoyaa@udea.edu.co>
2019-03-06 18:32:41 -0500

    Se cambia URL

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB
 if(process.env.NODE_ENV=== 'dev'){
     urlDB ='mongodb://localhost:27017/cafe'
 }else{
-    urlDB= "mongodb+srv://mongo:1234@cluster0-3vt2r.mongodb.net/test?retryWrites=true"  
+    urlDB= process.env.mongoDB  
 }
 process.env.urlDB= urlDB
